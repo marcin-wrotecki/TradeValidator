d0b8b039638ae4d6029fa9756f0fc7350e9eabd2
marcin-wrotecki <marcin.wrotecki.97@gmail.com>
2022-07-06 21:23:15 +0200

    Initialize project

---
+++ src/main/java/com/ubs/TradeValidatorApplication.java
@@ -0,0 +1,13 @@
+package com.ubs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TradeValidatorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TradeValidatorApplication.class, args);
+	}
+
+}
