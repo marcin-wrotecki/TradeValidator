6d99a97cb8c9604999e2c3ad00d6b3d284c534a0
Wrotecki <mnrk@gft.com>
2022-07-07 10:34:27 +0200

    Add documentation to Swagger

--- src/main/java/com/ubs/config/SwaggerConfig.java
+++ src/main/java/com/ubs/config/SwaggerConfig.java
@@ -1,11 +1,34 @@
 package com.ubs.config;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+import springfox.documentation.builders.ApiInfoBuilder;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.service.Contact;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @Configuration
 @EnableSwagger2
 public class SwaggerConfig {
 
-}
+	@Bean
+	public Docket get() {
+		return new Docket(DocumentationType.SWAGGER_2)
+				.select()
+				.apis(RequestHandlerSelectors.basePackage("com.ubs.controller"))
+				.build().apiInfo(createApiInfo());
+	}
+	
+	private ApiInfo createApiInfo() {
+		return new ApiInfoBuilder()
+				.title("Trades API")
+				.description("The app that allows you to validate FX trades")
+				.version("1.0.0")
+				.contact(new Contact("Marcin Wrotecki", "\"https://github.com/marcin-wrotecki\"", "marcin.wrotecki@gft.com"))
+				.license("My own license")
+				.build();
+}	}
--- src/main/java/com/ubs/controller/TransactionController.java
+++ src/main/java/com/ubs/controller/TransactionController.java
@@ -16,40 +16,49 @@ import com.ubs.model.OptionTrade;
 import com.ubs.model.Trade;
 import com.ubs.service.ValidateService;
 
+import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiParam;
 
 @RestController
 public class TransactionController {
-	
+
 	private ValidateService validateService;
 	private ValidationProperties validationProperties;
-	
+
 	public TransactionController(ValidateService validateService, ValidationProperties validationProperties) {
 		this.validateService = validateService;
 		this.validationProperties = validationProperties;
 	}
 
-	
+	@ApiOperation(value = "Validates FX Spot or FX Forward trade", notes = "Provide input as JSON")
 	@PostMapping(value = "/trade/validate", consumes = MediaType.APPLICATION_JSON_VALUE)
-	public ResponseEntity<?> validateTrade(@RequestBody @Valid Trade trade, Errors errors) {
-		if(validationProperties.OPTION_TYPE.equalsIgnoreCase(trade.getType())){
+	public ResponseEntity<?> validateTrade(@ApiParam(value = "trade in JSON form") @RequestBody @Valid Trade trade,
+			Errors errors) {
+		if (validationProperties.OPTION_TYPE.equalsIgnoreCase(trade.getType())) {
 			return ResponseEntity.badRequest().body("Wrong endpoint for option trades");
 		}
 		return validateService.validate(trade, errors);
 	}
-	
+
+	@ApiOperation(value = "Validates collection of FX Spot or FX Forward trades", notes = "Provide input as JSON array")
 	@PostMapping(value = "/trades/validate", consumes = MediaType.APPLICATION_JSON_VALUE)
-	public ResponseEntity<?> validateTrades(@RequestBody List<@Valid Trade> trades) {
+	public ResponseEntity<?> validateTrades(
+			@ApiParam(value = "Collection of trades in JSON form") @RequestBody List<@Valid Trade> trades) {
 		return validateService.validate(trades);
 	}
 
+	@ApiOperation(value = "Validates FX Option trade", notes = "Provide input as JSON ")
 	@PostMapping(value = "/trade/option/validate", consumes = MediaType.APPLICATION_JSON_VALUE)
-	public ResponseEntity<?> validateTrade(@RequestBody @Valid OptionTrade trade, Errors errors) {
+	public ResponseEntity<?> validateTrade(
+			@ApiParam(value = "trade in JSON form") @RequestBody @Valid OptionTrade trade, Errors errors) {
 		return validateService.validate(trade, errors);
 	}
-	
+
+	@ApiOperation(value = "Validates collection of FX Option trades", notes = "Provide input as JSON array")
 	@PostMapping(value = "/trades/option/validate", consumes = MediaType.APPLICATION_JSON_VALUE)
-	public ResponseEntity<?> validateOptionTrades(@RequestBody List<@Valid OptionTrade> trades) {
+	public ResponseEntity<?> validateOptionTrades(
+			@ApiParam(value = "Collection of trades in JSON form") @RequestBody List<@Valid OptionTrade> trades) {
 		return validateService.validate(trades);
 	}
-	
+
 }
